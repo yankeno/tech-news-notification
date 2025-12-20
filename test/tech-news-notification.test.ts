808b1dc7859c343f9a93a80a6c653b95977ec0a1
yankeno <kn5sttyryh@gmail.com>
2025-12-20 20:52:24 +0900

    Initial commit

---
+++ test/tech-news-notification.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TechNewsNotification from '../lib/tech-news-notification-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tech-news-notification-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TechNewsNotification.TechNewsNotificationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
