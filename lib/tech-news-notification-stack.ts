66e0e0ebfcc58e4347e3b44d4a5985c7e2595c63
yankeno <kn5sttyryh@gmail.com>
2025-12-20 21:53:32 +0900

    add: Parameter Store

--- lib/tech-news-notification-stack.ts
+++ lib/tech-news-notification-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib/core";
+import { Construct } from "constructs";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as ssm from "aws-cdk-lib/aws-ssm";
 
 export class TechNewsNotificationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'TechNewsNotificationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const slackWebhookParams = new ssm.StringParameter(
+      this,
+      "SlackWebhookUrl",
+      {
+        parameterName: "/tech-news-notification/slack/webhook-url",
+        stringValue: "REPLACE_ME",
+      }
+    );
   }
 }
